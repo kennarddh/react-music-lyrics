9e0367165377e7ce79c8269dfcda481351b02c1c
kennarddh <79390992+kennarddh@users.noreply.github.com>
2023-09-06 20:39:56 +0700

    feat: Sidebar change to full screen button

--- src/Components/Sidebar/Sidebar.tsx
+++ src/Components/Sidebar/Sidebar.tsx
@@ -1,15 +1,21 @@
 import { FC } from 'react'
 
+import useFullScreen from 'Hooks/useFullScreen'
+
 import { Container, Label, Input, Button } from './Styles'
 
 const Sidebar: FC = () => {
+	const { SetIsFullScreen } = useFullScreen()
+
 	return (
 		<Container>
 			<Label htmlFor='fontSize'>
 				Font size
 				<Input id='fontSize' type='number' min={0} max={100} />
 			</Label>
-			<Button>Change to full screen</Button>
+			<Button onClick={() => SetIsFullScreen(true)}>
+				Change to full screen
+			</Button>
 		</Container>
 	)
 }
