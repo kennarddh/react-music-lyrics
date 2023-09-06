d4d83ba2b60d1b93d93d63add704cedd71f1baea
kennarddh <79390992+kennarddh@users.noreply.github.com>
2023-09-06 05:56:19 -0700

    Initial commit

---
+++ src/env.d.ts
@@ -0,0 +1,9 @@
+/// <reference types="vite/client" />
+
+// Declare env types here
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+interface ImportMetaEnv {}
+
+interface ImportMeta {
+	readonly env: Readonly<ImportMetaEnv>
+}
---
+++ vite.config.ts
@@ -0,0 +1,63 @@
+import { defineConfig, loadEnv } from 'vite'
+
+import react from '@vitejs/plugin-react'
+
+import { checker } from 'vite-plugin-checker'
+import svgr from 'vite-plugin-svgr'
+
+import { resolve } from 'path'
+
+export const relativeAlias: Record<string, string> = {
+	Components: './src/Components',
+	Contexts: './src/Contexts',
+	Utils: './src/Utils',
+	Hooks: './src/Hooks',
+	Constants: './src/Constants',
+	Api: './src/Api',
+	Pages: './src/Pages',
+}
+
+export const resolveAlias = Object.entries(relativeAlias).reduce(
+	(prev: Record<string, string>, [key, path]) => {
+		// eslint-disable-next-line security/detect-object-injection
+		prev[key] = resolve(__dirname, path)
+
+		return prev
+	},
+	{}
+)
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+	const envPrefix: string[] = ['APP_']
+
+	const { PORT = 3000, OPEN_BROWSER = 'true' } = {
+		...loadEnv(mode, process.cwd(), ''),
+	}
+
+	return {
+		plugins: [
+			react(),
+			svgr(),
+			mode === 'development'
+				? checker({
+						typescript: true,
+						eslint: {
+							lintCommand: 'lint:check',
+						},
+				  }) // eslint-disable-line no-mixed-spaces-and-tabs
+				: null,
+		],
+		resolve: {
+			alias: resolveAlias,
+		},
+		server: {
+			port: PORT || 3000,
+			open: OPEN_BROWSER === 'true' ? true : false,
+		},
+		envPrefix,
+		build: {
+			outDir: 'build',
+		},
+	}
+})
