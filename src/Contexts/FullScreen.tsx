fd63a8ae2326c1c115e6544e290f1b4ed7b1236c
kennarddh <79390992+kennarddh@users.noreply.github.com>
2023-09-07 10:30:45 +0700

    feat: Full screen context element

--- src/Contexts/FullScreen.tsx
+++ src/Contexts/FullScreen.tsx
@@ -1,8 +1,16 @@
-import { FC, ReactNode, createContext, useCallback, useState } from 'react'
+import {
+	FC,
+	ReactNode,
+	createContext,
+	useCallback,
+	useRef,
+	useState,
+} from 'react'
 
 interface IPagesContext {
 	IsFullScreen: boolean
 	SetIsFullScreen: (title: boolean) => void
+	SetFullScreenElement: (element: HTMLElement) => void
 }
 
 interface IPagesContextProvider {
@@ -12,10 +20,12 @@ interface IPagesContextProvider {
 const FullScreenContext = createContext<IPagesContext>({
 	IsFullScreen: false,
 	SetIsFullScreen: () => undefined,
+	SetFullScreenElement: () => undefined,
 })
 
 export const FullScreenProvider: FC<IPagesContextProvider> = ({ children }) => {
 	const [IsFullScreen, SetIsFullScreenBase] = useState<boolean>(false)
+	const FullScreenElement = useRef<HTMLElement>()
 
 	const SetIsFullScreen = useCallback((isFullScreen: boolean) => {
 		if (!navigator.userActivation.isActive) return
@@ -30,8 +40,14 @@ export const FullScreenProvider: FC<IPagesContextProvider> = ({ children }) => {
 		SetIsFullScreenBase(isFullScreen)
 	}, [])
 
+	const SetFullScreenElement = useCallback((element: HTMLElement) => {
+		FullScreenElement.current = element
+	}, [])
+
 	return (
-		<FullScreenContext.Provider value={{ IsFullScreen, SetIsFullScreen }}>
+		<FullScreenContext.Provider
+			value={{ IsFullScreen, SetIsFullScreen, SetFullScreenElement }}
+		>
 			{children}
 		</FullScreenContext.Provider>
 	)
