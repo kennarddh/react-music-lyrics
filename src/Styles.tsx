48716d80ce4f725c1c9637f1b45e60eac63979fe
kennarddh <79390992+kennarddh@users.noreply.github.com>
2023-09-07 12:51:12 +0700

    style: Add space global styles

--- src/Styles.tsx
+++ src/Styles.tsx
@@ -7,7 +7,7 @@ const GlobalStyle = createGlobalStyle`
 		margin: 0;
 		padding: 0;
 		box-sizing: border-box;
-		font-family:${FontFamilyGenericNames.SansSerif};
+		font-family: ${FontFamilyGenericNames.SansSerif};
 	}
 `
 
