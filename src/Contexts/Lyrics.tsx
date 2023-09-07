95a82cea5e09dee52edd56c08ff789901208c1f3
kennarddh <79390992+kennarddh@users.noreply.github.com>
2023-09-07 13:55:19 +0700

    feat: Lyrics context state

--- src/Contexts/Lyrics.tsx
+++ src/Contexts/Lyrics.tsx
@@ -1,4 +1,4 @@
-import { FC, ReactNode, createContext } from 'react'
+import { FC, ReactNode, createContext, useState, useCallback } from 'react'
 
 import { UUID } from 'Types'
 
@@ -8,17 +8,50 @@ export interface ILyricSegment {
 	words: string
 }
 
-interface ILyricsContext {}
+interface ILyricsContext {
+	LyricSegments: ILyricSegment[]
+
+	AddLyricSegment: (newSegment: ILyricSegment) => void
+	EditLyricSegment: (id: UUID, newSegment: Omit<ILyricSegment, 'id'>) => void
+}
 
 interface ILyricsContextProvider {
 	children: ReactNode
 }
 
-const LyricsContext = createContext<ILyricsContext>({})
+const LyricsContext = createContext<ILyricsContext>({
+	LyricSegments: [],
+
+	AddLyricSegment: () => undefined,
+	EditLyricSegment: () => undefined,
+})
 
 export const LyricsProvider: FC<ILyricsContextProvider> = ({ children }) => {
+	const [LyricSegments, SetLyricSegments] = useState<ILyricSegment[]>([])
+
+	const EditLyricSegment = useCallback(
+		(id: UUID, newSegment: Omit<ILyricSegment, 'id'>) => {
+			SetLyricSegments(prev =>
+				prev.map(iterSegment =>
+					iterSegment.id === id
+						? { id, ...newSegment }
+						: { ...iterSegment },
+				),
+			)
+		},
+		[],
+	)
+
+	const AddLyricSegment = useCallback((newSegment: ILyricSegment) => {
+		SetLyricSegments(prev => [...prev, newSegment])
+	}, [])
+
 	return (
-		<LyricsContext.Provider value={{}}>{children}</LyricsContext.Provider>
+		<LyricsContext.Provider
+			value={{ LyricSegments, AddLyricSegment, EditLyricSegment }}
+		>
+			{children}
+		</LyricsContext.Provider>
 	)
 }
 
