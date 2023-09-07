858a906153bc43520c49ffdf8e7998df24c5d022
kennarddh <79390992+kennarddh@users.noreply.github.com>
2023-09-07 12:27:13 +0700

    feat: Content font family

--- src/Components/Scene/Scene.tsx
+++ src/Components/Scene/Scene.tsx
@@ -7,13 +7,20 @@ import useContentStyles from 'Hooks/useContentStyles'
 
 const Scene: FC = () => {
 	const { FullScreenElementRef } = useFullScreen()
-	const { FontSize, TextColor } = useContentStyles()
+	const { FontSize, TextColor, TextFontFamilyGenericName } =
+		useContentStyles()
 
 	return (
 		<Container>
 			<Content ref={FullScreenElementRef}>
 				<BackgroundImage src='/background.jpg' />
-				<Text style={{ fontSize: FontSize, color: TextColor }}>
+				<Text
+					style={{
+						fontSize: FontSize,
+						color: TextColor,
+						fontFamily: TextFontFamilyGenericName,
+					}}
+				>
 					Text
 				</Text>
 			</Content>
