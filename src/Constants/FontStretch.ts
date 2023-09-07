c73417b2a7c1a4a9281b3b9bf15a8a5e45de1b22
kennarddh <79390992+kennarddh@users.noreply.github.com>
2023-09-08 01:29:39 +0700

    feat: Font stretch constant

---
+++ src/Constants/FontStretch.ts
@@ -0,0 +1,13 @@
+enum FontStretch {
+	Normal = 'normal',
+	UultraCondensed = 'ultra-condensed',
+	ExtraCondensed = 'extra-condensed',
+	Condensed = 'condensed',
+	SemiCondensed = 'semi-condensed',
+	SemiExpanded = 'semi-expanded',
+	Expanded = 'expanded',
+	ExtraExpanded = 'extra-expanded',
+	UltraExpanded = 'ultra-expanded',
+}
+
+export default FontStretch
