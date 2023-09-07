363576320952044b6a8980632ce53406972fa77f
kennarddh <79390992+kennarddh@users.noreply.github.com>
2023-09-07 12:33:39 +0700

    feat: Change page font family to sans serif

--- src/Styles.tsx
+++ src/Styles.tsx
@@ -1,10 +1,13 @@
 import { createGlobalStyle } from 'styled-components'
 
+import FontFamilyGenericNames from 'Constants/FontFamilyGenericNames'
+
 const GlobalStyle = createGlobalStyle`
 	* {
 		margin: 0;
 		padding: 0;
 		box-sizing: border-box;
+		font-family:${FontFamilyGenericNames.SansSerif};
 	}
 `
 
