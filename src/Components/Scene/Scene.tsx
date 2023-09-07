4e815003b4f16ce3461b95c9ac71b18c4a8388ca
kennarddh <79390992+kennarddh@users.noreply.github.com>
2023-09-07 11:58:49 +0700

    feat: Content text style based on content styles context

--- src/Components/Scene/Scene.tsx
+++ src/Components/Scene/Scene.tsx
@@ -3,15 +3,19 @@ import { FC } from 'react'
 import { Container, Content, BackgroundImage, Text } from './Styles'
 
 import useFullScreen from 'Hooks/useFullScreen'
+import useContentStyles from 'Hooks/useContentStyles'
 
 const Scene: FC = () => {
 	const { FullScreenElementRef } = useFullScreen()
+	const { FontSize, TextColor } = useContentStyles()
 
 	return (
 		<Container>
 			<Content ref={FullScreenElementRef}>
 				<BackgroundImage src='/background.jpg' />
-				<Text>Text</Text>
+				<Text style={{ fontSize: FontSize, color: TextColor }}>
+					Text
+				</Text>
 			</Content>
 		</Container>
 	)
