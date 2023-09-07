2cb81ea162552658a4d25fefb7c26aaf920a52ef
kennarddh <79390992+kennarddh@users.noreply.github.com>
2023-09-07 12:09:18 +0700

    feat(constants): Font family generic names

---
+++ src/Constants/FontFamilyGenericNames.ts
@@ -0,0 +1,17 @@
+enum FontFamilyGenericNames {
+	Serif = 'serif',
+	SansSerif = 'sans-serif',
+	Monospace = 'monospace',
+	Cursive = 'cursive',
+	Fantasy = 'fantasy',
+	SystemUI = 'system-ui',
+	UISerif = 'ui-serif',
+	UISansSerif = 'ui-sans-serif',
+	UIMonospace = 'ui-monospace',
+	UIRounded = 'ui-rounded',
+	Emoji = 'emoji',
+	Math = 'math',
+	Fangsong = 'fangsong',
+}
+
+export default FontFamilyGenericNames
