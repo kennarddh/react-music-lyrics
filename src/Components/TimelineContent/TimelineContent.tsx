2e8ed3ba06e8311de550a1899d19c44855b8d725
kennarddh <79390992+kennarddh@users.noreply.github.com>
2023-09-08 23:13:25 +0700

    feat: Drag timeline content

--- src/Components/TimelineContent/TimelineContent.tsx
+++ src/Components/TimelineContent/TimelineContent.tsx
@@ -1,8 +1,11 @@
 import { FC } from 'react'
 
 import { UUID } from 'Types'
+import { useDrag } from 'react-dnd'
 
 import useLyric from 'Hooks/useLyric'
+
+import DndItemTypes from 'Constants/DndItemTypes'
 
 import { Content } from './Styles'
 
@@ -10,8 +13,18 @@ const TimelineContent: FC<{ segmentID: UUID }> = ({ segmentID }) => {
 	const { CurrentSegmentID, SetCurrentSegmentID, GetLyricSegmentByID } =
 		useLyric()
 
+	const [{ isDragging }, drag] = useDrag(() => ({
+		type: DndItemTypes.TimelineContent,
+		collect: monitor => ({
+			isDragging: !!monitor.isDragging(),
+		}),
+	}))
+
+	if (isDragging) return null
+
 	return (
 		<Content
+			ref={drag}
 			onClick={() => SetCurrentSegmentID(segmentID)}
 			$isActive={segmentID === CurrentSegmentID}
 		>
